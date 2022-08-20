04a06ff71b265ebca278d675e693a11b797fdfa0
Li, Haixi <ethorondor@gmail.com>
2022-08-19 22:01:41 -0400

    master

--- code/main.py
+++ code/main.py
@@ -1,5 +1,25 @@
 #%%
 '''
+424 longest repeating character replacement
+'''
+class solution:
+    def character_replacement(self, s, k):
+        count = {}
+        res = 0
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r],0) # get the count of s[r], if no, return 0
+            while (r-l+1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r-l+1)
+        return res
+s = "AABABBA"
+k = 2
+solution = solution()
+solution.character_replacement(s,k)
+#%%
+'''
 11 container with most water
 '''
 class solution:
