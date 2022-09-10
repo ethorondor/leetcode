e36a7adebbb1d6ac7aa8f9b66be3568bb534c3c3
Haixi Li <ethorondor@gmail.com>
2022-09-10 17:10:43 -0400

    master

---
+++ code/subsets.py
@@ -0,0 +1,16 @@
+'''
+find all subsets
+'''
+def find_subsets(nums):
+    subset = []
+    subset.append([])
+    for number in nums:
+        len_subset = len(subset)
+        for i in range(len_subset):
+            set1 = list(subset[i])
+            set1.append(number)
+            subset.append(set1)
+    return subset
+
+nums = [1,3,5]
+find_subsets(nums)
