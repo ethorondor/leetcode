ce7085fd790087e24c06d20697680e654a348b42
Li, Haixi <ethorondor@gmail.com>
2022-09-17 23:12:45 -0400

    master

--- review/max_sum_subarray_of_size_k.py
+++ review/max_sum_subarray_of_size_k.py
@@ -1,6 +1,7 @@
 '''
 maximum sum of subarray of size k
 '''
+#%%
 class solutions:
     def max_sum_subarray(self, nums, k):
         ans = 0
@@ -8,3 +9,12 @@ class solutions:
         sums = 0
         for i in range(k):
             sums += nums[i]
+        for n in range(k, len(nums)):
+            sums = sums + nums[n] - nums[n-k]
+            ans = max(ans, sums)
+        return ans
+nums = [1,4,2,6,9]
+k = 2
+s = solutions()
+s.max_sum_subarray(nums, k)
+# %%
---
+++ review/smallest_subarray_with_a_greated_sum.py
@@ -0,0 +1,27 @@
+'''
+smallest subarry with a greated sum
+'''
+import math
+class solutions:
+    def smallest_subarray(self, nums, target):
+        ans = []
+        l = 0
+        _sum = nums[0]
+        _len = math.inf
+        r = 0
+        while r < len(nums)-1:
+            if _sum < target:
+                r += 1
+                _sum = _sum + nums[r]
+            else:
+                if r - l + 1 < _len:
+                    _len = r - l +1
+                    ans = nums[l:r+1]
+                _sum = _sum - nums[l]
+                l += 1
+        return ans
+ 
+nums = [2,1,5,2,3,2]
+target = 7   
+s = solutions()
+s.smallest_subarray(nums, target)
