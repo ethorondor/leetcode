c551183610b4bd44b0e0fe4f8d77049cd98b56c3
Haixi Li <ethorondor@gmail.com>
2022-09-20 22:05:48 -0400

    master

---
+++ review/longest_substring.py
@@ -0,0 +1,24 @@
+'''
+longest substring
+'''
+#%%
+class solutions:
+    def longest_substring(self, s):
+        l = 0
+        ans = 0
+        sub_string = {}
+        for r in range(len(s)):
+            if s[r] not in sub_string:
+                sub_string[s[r]] = 0
+            sub_string[s[r]] += 1
+            while sub_string[s[r]] > 1:
+                sub_string[s[l]] -= 1
+                if sub_string[s[l]] == 0:
+                    del sub_string[s[l]]
+                l += 1
+            ans = max(ans, (r-l+1))
+        return ans
+s = 'aabbabcc'
+solution = solutions()
+solution.longest_substring(s)
+# %%
