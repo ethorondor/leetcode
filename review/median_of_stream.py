5d0fe3c60943fa36c8c812b03740e9ab9e3d28b8
Haixi Li <ethorondor@gmail.com>
2022-09-27 17:35:46 -0400

    master

---
+++ review/median_of_stream.py
@@ -0,0 +1,18 @@
+'''
+define a class that will return median of a stream
+'''
+#%%
+class median_of_stream:
+    def __init__(self):
+        pass
+    
+    def insert(self, num):
+        self.append(num)
+        return self
+    
+    def find_median(self):
+        return self
+    
+s = median_of_stream()
+s.insert(1)
+# %%
