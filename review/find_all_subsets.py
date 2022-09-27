8ecc077fd7cb6b80882a96fcdfb23c84b34af6e3
Haixi Li <ethorondor@gmail.com>
2022-09-27 19:28:38 -0400

    master

--- review/find_all_subsets.py
+++ review/find_all_subsets.py
@@ -4,5 +4,14 @@ find all subsets
 class solutions:
     def subsets(self, nums):
         ans = []
-        for i in range(len(nums)):
-            
+        ans.append([])
+        for i in nums:
+            n = len(ans)
+            for j in range(n):
+                sub1 = list(ans[j])
+                sub1.append(i)
+                ans.append(sub1)
+        return ans
+nums = [1,3]
+s = solutions()
+s.subsets(nums)
