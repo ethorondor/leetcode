8724f7625469582dc75640a7fe33ede5dfa5a92f
Haixi Li <ethorondor@gmail.com>
2022-10-14 09:51:11 -0400

    master

--- review/reverse_to_make_equal.py
+++ review/reverse_to_make_equal.py
@@ -1,7 +1,15 @@
 '''
 reverse to make equal
 '''
+#%%
 def are_they_equal(array_a, array_b):
+  if array_a is None and array_b is None:
+    return True
+  elif array_a is None:
+    return False
+  elif array_b is None:
+    return False
+  
       # Write your code here
   hash_table = {}
   for n in array_a:
@@ -13,9 +21,7 @@ def are_they_equal(array_a, array_b):
       hash_table[n] -= 1
     else:
       return False
-  if sum(hash_table.values()) == 0:
-    return True
-  else:
-    return False
+  return (sum(hash_table.values()) == 0)
 
 are_they_equal([1,2,3,4],[1,3,2,4])
+# %%
