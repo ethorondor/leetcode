1720454e7cc73028d2e1b367cfe7e7c279de1180
Haixi Li <ethorondor@gmail.com>
2022-10-15 18:55:05 +0000

    master

--- code/tree/level_order_traversal_binary_tree.py
+++ code/tree/level_order_traversal_binary_tree.py
@@ -1,6 +1,8 @@
 '''
 level order traversal binary tree
 '''
+#%%
+from collections import deque
 class node:
     def __init__(self, value):
         self.value = value
@@ -14,3 +16,22 @@ root.left.left = node(1)
 root.left.right = node(3)
 root.right.left = node(6)
 root.right.right = node(9)
+
+def level_order_traversal(root):
+    q = deque()
+    q.append(root)
+    res = []
+    while q:
+        _len = len(q)
+        level = []
+        for _ in range(_len):
+            _node = q.popleft()
+            level.append(_node.value)
+            if _node.left:
+                q.append(_node.left)
+            if _node.right:
+                q.append(_node.right)
+        res.append(level)
+    return res
+level_order_traversal(root)
+# %%
---
+++ code/tree/righ_side_view_binary_tree.py
@@ -0,0 +1,28 @@
+'''
+right side view of binary tree
+'''
+#%%
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def right_side(root):
+    res = []
+    def dfs(root):
+        if root is None:
+            return None
+        res.append(root.value)
+        right = dfs(root.right)
+    dfs(root)
+    return res
+root = node(4)
+root.left = node(2)
+root.right = node(7)
+root.left.left = node(1)
+root.left.right = node(3)
+root.right.left = node(6)
+root.right.right = node(9)
+right_side(root)
+# %%
