68acec229748e70d49552ed3896b31eebed14d23
Haixi Li <ethorondor@gmail.com>
2022-10-30 08:59:53 -0400

    master

--- code/string/basic_calculator.py
+++ code/string/basic_calculator.py
@@ -1,31 +1,39 @@
 '''
 basic calculator
-input: s = "(1+(4+5+2)-3)+(6+8)"
+input: s = "(23+(4+5+2)-3)+(6+8)"
 '''
+#%%
 class solution:
     def calculation(self, s):
-        cur = res = 0
+        _num = 0
+        _sum = 0
         sign = 1
         stack = []
         
         for char in s:
+            
             if char.isdigit():
-                cur = cur*10 + int(char)
+                _num = _num*10 + int(char)
             elif char in ['+','-']:
-                res += sign * cur
-                sign = 1 if char == '+' else -1
-                cur = 0
+                _sum += sign * _num
+                if char == '+':
+                    sign = 1
+                else:
+                    sign = -1
+                _num = 0
             elif char == '(':
-                stack.append(res)
+                stack.append(_sum)
                 stack.append(sign)
                 sign = 1
-                res = 0
+                _sum = 0
             elif char == ')':
-                res += sign*cur
-                res *= stack.pop()
-                res += stack.pop()
-                cur = 0
-        return res + sign*cur
-s = "(1+(4+5+2)-3)+(6+8)"
+                _sum += sign*_num
+                _sum *= stack.pop()
+                _sum += stack.pop()
+                _num = 0
+        return _sum + sign*_num
+#s = "(23+(4+5+2)-3)+(6+8)"
+s = '2+3+9-29'
 solution = solution()
 solution.calculation(s)
+# %%
