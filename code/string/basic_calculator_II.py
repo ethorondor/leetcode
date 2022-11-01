a9657b7a16c435394368f0a07ccec0f56105b03d
Haixi Li <ethorondor@gmail.com>
2022-11-01 14:44:04 +0000

    master

---
+++ code/string/basic_calculator_II.py
@@ -0,0 +1,29 @@
+'''
+s = '2 + 2*3 - 5 +4/5'
+'''
+#%%
+class solutions:
+    def calculation(self, s):
+        num = 0
+        sign = '+'
+        stack = []
+        for i in s:
+            if i.isdigit():
+                num = num*10 + int(i)
+            elif i in '+-*/':
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop()*num)
+                elif sign == '/':
+                    stack.append(int(stack.pop()/num))
+                sign = i
+                num = 0
+        stack.append(num)
+        return sum(stack)  
+s = '2+2*3-5+4/4'
+solution = solutions()
+solution.calculation(s)
+# %%
