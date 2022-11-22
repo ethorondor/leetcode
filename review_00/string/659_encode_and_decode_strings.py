bf395988341e02c92f7412992c5ae885ff3e6b8a
Haixi Li <ethorondor@gmail.com>
2022-11-21 21:53:54 -0500

    master

---
+++ review_00/string/659_encode_and_decode_strings.py
@@ -0,0 +1,30 @@
+'''
+659 encode and decode strings
+design an algorithm to encode a list of strings to a string. The encode string is then sent over the network and is decoded back to the original list of string.
+'''
+#%%
+class solutions:
+    def encoder(self, strs):
+        res = ''
+        for s in strs:
+            res += str(len(s)) + '#' + s
+        return res
+    
+    def decoder(self, str):
+        res = []
+        i = 0
+        
+        while i < len(str):
+            j = i
+            while str[j] != '#':
+                j += 1
+            length = int(str[i:j])
+            res.append(str[j+1:j+1+length])
+            i = j+1+length
+        return res
+    
+strs = ['hello', 'world']
+sln = solutions()
+sln.encoder(strs)
+sln.decoder(sln.encoder(strs))
+# %%
